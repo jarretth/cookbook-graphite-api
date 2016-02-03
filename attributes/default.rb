42ce95fd461ff44d392bccc0630428f14956be22
jarretth <jarrett.hawrylak@showclix.com>
2016-02-03 10:38:44 -0500

    gunicorn path on rhel

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,6 +15,13 @@ if node['graphite_api']['whisper']['enabled'] == true
   default['graphite_api']['finders'] |= ['graphite_api.finders.whisper.WhisperFinder']
 end
 
+case node['platform_family']
+when 'debian'
+    default['graphite_api']['gunicorn_path'] = '/usr/local/bin/gunicorn'
+when 'rhel'
+    default['graphite_api']['gunicorn_path'] = '/usr/bin/gunicorn'
+end
+
 default['graphite_api']['gunicorn_path'] = '/usr/local/bin/gunicorn'
 default['graphite_api']['gunicorn_workers'] = 1
 default['graphite_api']['bind_address'] = '0.0.0.0'
